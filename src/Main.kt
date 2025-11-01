9c3c3863ce167439ea52da83b8df03de553ad457
whdmldus1116 <76983867+whdmldus1116@users.noreply.github.com>
2025-11-01 13:38:02 +0900

    test commit

---
+++ src/Main.kt
@@ -0,0 +1,16 @@
+//TIP 코드를 <b>실행</b>하려면 <shortcut actionId="Run"/>을(를) 누르거나
+// 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
+fun main() {
+
+    val s = "ABC"
+    val i = 1
+    val l = 1L
+    val d = 1.0
+    val f = 1.0f
+
+    println("s = " + s::class)
+    println("i = " + i::class)
+    println("l = " + l::class)
+    println("d = " + d::class)
+    println("f = " + f::class)
+}
